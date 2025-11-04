890eb54bddc3e4b84e306b9d8f03eed67198de10
gmontesaa <158535358+gmontesaa@users.noreply.github.com>
2025-11-04 10:11:55 -0500

    Delete IAroutecraft directory

--- IAroutecraft/routecraft/places/ports.py
+++
@@ -1,7 +0,0 @@
-from typing import Protocol, List, Tuple, Dict
-
-class RoutingClient(Protocol):
-    def route(self, waypoints: List[Tuple[float, float]]) -> Dict: ...
-
-class EmbeddingsClient(Protocol):
-    def embed(self, texts: List[str]) -> List[List[float]]: ...
